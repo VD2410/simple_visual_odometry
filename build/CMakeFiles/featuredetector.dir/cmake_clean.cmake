57912b4c57588703da99fe240fb68bca4c60ac96
VD2410 <vishalbatavia88@yahoo.com>
2018-11-13 15:01:33 -0800

    Add files via upload

---
+++ build/CMakeFiles/featuredetector.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/featuredetector.dir/test/featuredetector.cpp.o"
+  "featuredetector.pdb"
+  "featuredetector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/featuredetector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/frontend/OccupancyGrid.cpp
@@ -0,0 +1,34 @@
+#include "frontend/OccupancyGrid.h"
+using namespace std;
+using namespace cv;
+
+OccupancyGrid::OccupancyGrid()
+{
+	initializer();
+	// initializer1();
+}
+
+void OccupancyGrid::initializer1()
+{
+
+}
+
+void OccupancyGrid::setImageSize1(size_t cols, size_t rows)
+{
+
+}
+
+void OccupancyGrid::addPoint1(Point2f& p)
+{
+}
+
+bool OccupancyGrid::isNewFeature1(Point2f& p)
+{
+	return true;
+}
+
+void OccupancyGrid::resetGrid1()
+{
+
+}
+
